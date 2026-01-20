2c098296a11b995ce93e58cee822fa336deaaa77
Tanuja Arya <tanujaarya971@gmail.com>
2026-01-20 04:58:34 -0500

    Add files via upload
    Initial uplode

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# prsonal_intro_project
-my first python project 
+# personal introduction program
+This is a simple python program
+## how to run
+1.Open terminal in the folder
+2.Run:python personal_intro.py
+
+## Author
+Tanuja Arya 
---
+++ personal_intro.py
@@ -0,0 +1,7 @@
+name = input("What is your name")
+age = input("How old are you?")
+hobby = input("What is your favorite hobby?")
+
+print ("\n welcome", name + "!")
+print("you are",age,"year old and love",hobby+".")
+print("nice to meet you!")
